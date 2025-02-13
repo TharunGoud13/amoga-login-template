8903add22f086ef3376606dc2577b2785716abb3
TharunGoud13 <amaravaitharun13@gmail.com>
2025-02-13 11:14:10 +0530

    changed dobox font-size

--- src/components/DoBox/DoBox.tsx
+++ src/components/DoBox/DoBox.tsx
@@ -106,68 +106,70 @@ const DoBox = () => {
               .map((item: any) => (
                 <div key={item.msg_id}>
                   {item?.ref_msg_id === null && (
-                    <Card key={item.msg_id} className="py-1.5 px-1.5">
-                      <CardContent className="space-y-1 px-1.5 py-1.5">
-                        <p className="flex items-center gap-2 text-sm">
-                          <LuChartNoAxesGantt className="h-3.5 w-3.5 text-muted-foreground" />
+                    <Card key={item.msg_id} className="py-2 px-2">
+                      <CardContent className="space-y-[10px] px-2 py-2">
+                        <p className="flex items-center gap-2 text-md">
+                          <LuChartNoAxesGantt className="h-6 w-6 text-muted-foreground" />
                           {item.plan_name}
                         </p>
-                        <p className="flex items-center gap-2 text-sm">
-                          <LuChartNoAxesGantt className="h-3.5 w-3.5 text-muted-foreground" />
-                          <span>{item.plan_phase_name}</span>
+                        <p className="flex items-center gap-2 text-md">
+                          <LuChartNoAxesGantt className="h-6 w-6 text-muted-foreground" />
+                          <span className="text-md">
+                            {item.plan_phase_name}
+                          </span>
                         </p>
-                        <p className="flex items-center gap-2 text-sm">
-                          <CircleCheck className="h-3.5 w-3.5 text-muted-foreground" />
+                        <p className="flex items-center gap-2 text-md">
+                          <CircleCheck className="h-6 w-6 text-muted-foreground" />
                           <span>{item.task_title}</span>
                         </p>
-                        <p className="flex items-center gap-2 text-sm">
-                          <File className="h-3.5 w-3.5 text-muted-foreground" />
+                        <p className="flex items-center gap-2 text-md">
+                          <File className="h-6 w-6text-muted-foreground" />
                           <span>{item.mydoc_name}</span>
                         </p>
 
-                        <p className="flex items-center gap-2 text-sm">
-                          <Calendar className="h-3.5 w-3.5 text-muted-foreground" />
+                        <p className="flex items-center gap-2 text-md">
+                          <Calendar className="h-6 w-6 text-muted-foreground" />
                           <span>
                             Date:{" "}
                             {new Date(item.created_date).toLocaleDateString()}
                           </span>
                         </p>
-                        <p className="flex items-center gap-2 text-sm">
-                          <User className="h-3.5 w-3.5 text-muted-foreground" />
+                        <p className="flex items-center gap-2 text-md">
+                          <User className="h-6 w-6 text-muted-foreground" />
 
                           <span>From: {item.created_user_name}</span>
                         </p>
-                        <p className="flex font-semibold items-center gap-2 text-sm">
+                        <p className="flex font-semibold items-center gap-2 text-md">
                           <span>Subject: {item.msg_subject}</span>
                         </p>
-                        <p className="flex items-center text-muted-foreground gap-2 text-sm">
+                        <p className="flex items-center text-muted-foreground gap-2 text-md">
                           <span>Msg Group: {item.msg_group}</span>
                         </p>
-                        <p className="flex items-center text-muted-foreground gap-2 text-sm">
+                        <p className="flex items-center text-muted-foreground gap-2 text-md">
                           <span>Desc: {item.msg_description}</span>
                         </p>
 
                         <div className="flex justify-between items-center">
-                          <p className="flex items-center gap-2 text-sm">
-                            <LuCopyCheck className="h-3.5 w-3.5 text-muted-foreground" />
+                          <p className="flex items-center gap-2 text-md">
+                            <LuCopyCheck className="h-6 w-6 text-muted-foreground" />
 
                             <span>{item.status}</span>
                           </p>
-                          <div className="flex space-x-2">
+                          <div className="flex gap-1.5 md:gap-2 space-x-2">
                             <Link href={`/DoBox/Docs/${item.msg_id}`}>
-                              <File className="h-3.5 w-3.5 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
+                              <File className="h-6 w-6 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
                             </Link>
                             <Link href={`/DoBox/Tasks/${item.msg_id}`}>
-                              <ClipboardCheck className="h-3.5 w-3.5 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
+                              <ClipboardCheck className="h-6 w-6 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
                             </Link>
                             <Link href={`/DoBox/edit/${item.msg_id}`}>
-                              <Edit className="h-3.5 w-3.5 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
+                              <Edit className="h-6 w-6 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
                             </Link>
                             <Link href={`/DoBox/view/${item.msg_id}`}>
-                              <Eye className="h-3.5 w-3.5 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
+                              <Eye className="h-6 w-6 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
                             </Link>
                             <Link href={`/DoBox/Chat/${item.msg_id}`}>
-                              <LuMessageCircleMore className="h-3.5 w-3.5 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
+                              <LuMessageCircleMore className="h-6 w-6 text-muted-foreground stroke-[1.5] cursor-pointer hover:text-foreground" />
                             </Link>
                           </div>
                         </div>
