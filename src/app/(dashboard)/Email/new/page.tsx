577047c194233d8e8fc069ac97b8463863275565
TharunGoud13 <amaravaitharun13@gmail.com>
2025-04-04 17:34:38 +0530

    fixed props issue

--- src/app/(dashboard)/Email/new/page.tsx
+++ src/app/(dashboard)/Email/new/page.tsx
@@ -3,7 +3,7 @@ import NewEmail from "@/components/Email/NewEmail";
 const Page = () => {
   return (
     <div className="max-w-[800px] w-full h-full mx-auto p-4">
-      <NewEmail isEdit={false} />
+      <NewEmail isView={false} />
     </div>
   );
 };
