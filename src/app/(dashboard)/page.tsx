fa0f308bb1c4d42a5598935e7f0699126b5a1c0e
TharunGoud13 <amaravaitharun13@gmail.com>
2025-05-19 16:40:42 +0530

    changes

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,20 @@
 import { auth } from "@/auth";
 import UserInfo from "@/components/Capacitor/UserInfo";
 import LogoutButton from "@/components/forms/logout-button";
-import getUserLocation from "@/utils/getGeoLocation";
 import React from "react";
+import { Contacts, PermissionStatus } from "@capacitor-community/contacts";
 
 const Home = async () => {
   const session = await auth();
+  const permissionState: PermissionStatus = await Contacts.requestPermissions();
+  if (permissionState.contacts === "granted") {
+    console.log("Permission Granted");
+  }
+  const { contacts } = await Contacts.getContacts({
+    projection: { name: true, phones: true, image: true },
+  });
+
+  console.log("contacts-----------", contacts);
 
   return (
     <div className="flex flex-col bg-background/95 h-screen w-screen items-center justify-center">
