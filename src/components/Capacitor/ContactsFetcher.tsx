970809749ea8f485f67fe50b3988f61163d0d6c5
TharunGoud13 <amaravaitharun13@gmail.com>
2025-05-19 17:15:11 +0530

    cjanges

--- src/components/Capacitor/ContactsFetcher.tsx
+++ src/components/Capacitor/ContactsFetcher.tsx
@@ -6,9 +6,11 @@ import { Contacts } from "@capacitor-community/contacts";
 
 const ContactFetcher = () => {
   useEffect(() => {
+    console.log("contact---------------------");
     const fetchContacts = async () => {
       try {
         const permission = await Contacts.requestPermissions();
+        console.log("permission--------", permission.contacts);
         if (permission.contacts !== "granted") {
           console.error("Permission denied");
           return;
@@ -18,6 +20,8 @@ const ContactFetcher = () => {
         const { contacts } = await Contacts.getContacts({
           projection: { name: true, phones: true, image: true },
         });
+
+        console.log("hre----------");
 
         console.log("Contacts:----------", contacts);
       } catch (error) {
