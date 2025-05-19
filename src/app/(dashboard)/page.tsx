07590bf62426f0e1d02d8a10517609fc78e1dd16
TharunGoud13 <amaravaitharun13@gmail.com>
2025-05-19 15:48:07 +0530

    changes

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,10 +1,12 @@
 import { auth } from "@/auth";
 import UserInfo from "@/components/Capacitor/UserInfo";
 import LogoutButton from "@/components/forms/logout-button";
+import getUserLocation from "@/utils/getGeoLocation";
 import React from "react";
 
 const Home = async () => {
   const session = await auth();
+
   return (
     <div className="flex flex-col bg-background/95 h-screen w-screen items-center justify-center">
       <div>
--- src/components/Capacitor/UserInfo.tsx
+++ src/components/Capacitor/UserInfo.tsx
@@ -1,39 +1,61 @@
 "use client";
+import getUserLocation from "@/utils/getGeoLocation";
 import { useEffect, useState } from "react";
 
 declare global {
   interface Window {
     plugins: any;
+    cordova?: any;
   }
 }
 
 const SimInfo = () => {
   const [number, setNumber] = useState<string | null>(null);
   const [error, setError] = useState<string | null>(null);
+  const [location, setLocation] = useState<any>(null);
 
   useEffect(() => {
-    const getSimInfo = () => {
-      try {
-        if (!window.plugins?.sim) {
-          console.log("SIM plugin not found.");
-          setError("SIM plugin not available");
-          return;
-        }
+    const fetchLocation = async () => {
+      const location = await getUserLocation();
+      setLocation(location);
+    };
+    fetchLocation();
+  });
 
-        window.plugins.sim.getSimInfo(
-          (info: any) => {
-            console.log("SIM Info:", info);
-            setNumber(info.phoneNumber || "Number not available");
-          },
-          (err: any) => {
-            console.error("Error getting SIM info:", err);
-            setError("Failed to fetch SIM info");
-          }
-        );
-      } catch (e: any) {
-        console.error("Unexpected error caught:", e);
-        setError(e.message || "Unexpected error");
+  console.log("location-----", location);
+
+  useEffect(() => {
+    const getSimInfo = async () => {
+      if (!window.plugins?.sim) {
+        setError("SIM plugin not available");
+        return;
       }
+      console.log("window---plugin---", window.plugins.sim);
+
+      window.cordova.plugins.diagnostic.requestRuntimePermission(
+        (status: any) => {
+          console.log("status-----------", status);
+          if (status === "GRANTED") {
+            window.plugins.sim.getSimInfo(
+              (info: any) => {
+                console.log("info---------", info);
+                return setNumber(info.phoneNumber || "Number not available");
+              },
+              (err: any) => {
+                console.error("err--------", err);
+                setError("Failed to fetch SIM info");
+              }
+            );
+          } else {
+            setError("Permission denied");
+          }
+        },
+        (err: any) => {
+          console.error(err);
+          setError("Permission request failed");
+        },
+        "android.permission.READ_PHONE_NUMBERS"
+      );
     };
 
     getSimInfo();
@@ -44,6 +66,7 @@ const SimInfo = () => {
       <h3>SIM Info---------</h3>
       {number ? <p>Mobile Number: {number}</p> : <p>Loading...</p>}
       {error && <p style={{ color: "red" }}>{error}</p>}
+      <h1>User Location: {location?.address?.village}</h1>
     </div>
   );
 };
