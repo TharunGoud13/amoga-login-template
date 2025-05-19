0d89993bd865227dedd3c484d7e7d74b66bd1f33
TharunGoud13 <amaravaitharun13@gmail.com>
2025-05-19 11:13:48 +0530

    added capacitor and navigations

--- src/components/BackButtonHandler.tsx
+++ src/components/BackButtonHandler.tsx
@@ -2,30 +2,39 @@
 
 import { useEffect } from "react";
 import { App as CapApp } from "@capacitor/app";
-import { usePathname, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { PluginListenerHandle } from "@capacitor/core";
 
 const BackButtonHandler = () => {
-  const pathname = usePathname();
   const router = useRouter();
 
   useEffect(() => {
     let listenerHandle: PluginListenerHandle | null = null;
+
     CapApp.addListener("backButton", () => {
-      if (pathname === "/") {
+      const currentPath = window.location.pathname;
+      const canGoBack = window.history.length > 1;
+
+      console.log("Back button pressed on:", currentPath);
+
+      if (currentPath === "/" || !canGoBack) {
+        console.log("Exiting app...");
         CapApp.exitApp();
       } else {
+        console.log("Going back...");
         router.back();
       }
     }).then((handle) => {
       listenerHandle = handle;
     });
+
     return () => {
       if (listenerHandle) {
         listenerHandle.remove();
       }
     };
-  }, [pathname, router]);
+  }, [router]);
+
   return null;
 };
 
